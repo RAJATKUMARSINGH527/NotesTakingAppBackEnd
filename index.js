e13e454219437f7de887a26ac71fb5428463fe95
RAJATKUMARSINGH527 <rajatkumarsingh257@gmail.com>
2025-03-06 03:18:06 +0530

    Updated project

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const {connectToDB} = require('./config/db');
 const noteRouter = require('./routes/note.routes');
 const userRouter = require('./routes/user.routes');
 const cors = require('cors');
+require('dotenv').config();
 
 const swaggerJSDoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
@@ -47,36 +48,57 @@ const options = {
     apis: ["./routes/*.js"], // Path to the routes for API definitions
   };
   
-  const swaggerSpec = swaggerJSDoc(options);
+const swaggerSpec = swaggerJSDoc(options);
   
-  const app = express();
-  
-  // Swagger route
-  app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const app = express();
+
+// Swagger route
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
   
 
 
 //middleware to pass the request body
 app.use(express.json());
-app.use(cors(
-  {
-    origin: 'https://notes-taking-app-front-end.vercel.app',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-  }
-));   //to allow cross origin requests from the frontend to the backend server
+app.use(express.urlencoded({ extended: false })); //to pass the form data from the frontend to the backend server 
+
+// app.use(cors(
+//   {
+//     origin: 'https://notes-taking-app-front-end.vercel.app',
+//     credentials: true,
+//     methods: ['GET', 'POST', 'PUT', 'DELETE']
+//   }
+// ));   //to allow cross origin requests from the frontend to the backend server
 //  because the frontend and backend are running on different ports so we need to allow the cross origin requests 
 //  from the frontend to the backend server so that the frontend can communicate with the backend server 
+
+const whitelist = [process.env.FRONTEND_URL, process.env.DEPLOYED_FE_URL];
+
+const corsOptions = (req, callback) => {
+  if (whitelist.indexOf(req.header("Origin")) !== -1) {
+    callback(null, {
+      origin: req.header("Origin"), //// Automatically reflects the request's origin if in the whitelist
+      credentials: true,
+      methods: "GET,HEAD,PATCH,POST,PUT,DELETE",
+      allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    }); // reflect (enable) the requested origin in the CORS response
+  } else {
+    callback(null, {origin: false}); // Deny CORS if not in whitelist
+  }
+};
+app.use(cors(corsOptions));
+
+
+// app.use(cors())
 
 app.use('/notes', noteRouter);
 app.use('/users', userRouter);
 
+const PORT = process.env.PORT || 8000;
 
-
-app.listen(8000, async() => {
+app.listen(PORT, async() => {
     try {
         await connectToDB();
-        console.log('Server is running on port http://localhost:8000');
+        console.log(`Server is running on port http://localhost:${PORT}`);
     } catch (error) {   
         console.log('Error:', error);
     }
